a3d1c5f36b1d6c27d3968e2b0dbd97bf212b7b79
Denis Halupczok <denis.halupczok.external@atos.net>
2023-10-04 15:47:13 +0200

    Microfrontend approach implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPageComponent} from "./components/main-page/main-page.component";
+let routes: Routes = []
+ try {
+    routes = [{
+     path: '',
+     component: MainPageComponent,
+     pathMatch: "full",
+   },
+     // @ts-ignore
+     {
+       path: 'user-management',
+       // @ts-ignore
+       loadChildren: () => import('user-management-microfrontend-poc/Module').then(m => m.UserManagementModule).catch(err => {
+         console.warn(err)
+         return null;
+       })
+     },
+   ];
+ } catch (e) {
+   console.warn("Error:" , e)
+ }
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
