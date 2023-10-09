27a5f6b1b703b0a9558e654cc29d21f49a5a4efa
Denis Halupczok <denis.halupczok.external@atos.net>
2023-10-09 11:53:35 +0200

    Navbar implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from "./components/main-page/main-page.component";
+import { loadRemoteModule } from '@angular-architects/module-federation';
 let routes: Routes = []
  try {
     routes = [{
@@ -9,14 +10,23 @@ let routes: Routes = []
      pathMatch: "full",
    },
      // @ts-ignore
-     {
-       path: 'user-management',
-       // @ts-ignore
-       loadChildren: () => import('user-management-microfrontend-poc/Module').then(m => m.UserManagementModule).catch(err => {
-         console.warn(err)
-         return null;
-       })
-     },
+    //  {
+    //    path: 'user-management',
+    //    // @ts-ignore
+    //    loadChildren: () => import('user-management-microfrontend-poc/Module').then(m => m.UserManagementModule).catch(err => {
+    //      console.warn(err)
+    //      return null;
+    //    })
+    //  },
+    {
+      path: 'user-management',
+      // @ts-ignore
+      loadChildren: () => loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module'
+      }).then(m => m.UserManagementModule)
+    },
    ];
  } catch (e) {
    console.warn("Error:" , e)
